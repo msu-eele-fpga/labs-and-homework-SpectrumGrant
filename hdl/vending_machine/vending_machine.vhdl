1aa243642d142909133e42be6b23bb0fec543abf
Grant Kirkland <151691586+SpectrumGrant@users.noreply.github.com>
2024-09-11 16:52:44 -0600

    compiling output logic and state memory. started next state logic. untested

--- hdl/vending_machine/vending_machine.vhdl
+++ hdl/vending_machine/vending_machine.vhdl
@@ -1,9 +1,9 @@
 ----------------------------------------------------------------------------
--- Description:  Timed Counter component, when enabled counts for set time before outputting high for one clock cycle. If disabled outputs low.
+-- Description:  
 ----------------------------------------------------------------------------
 -- Author:       Grant Kirkland
 -- Company:      Montana State University
--- Create Date:  September 9, 2024
+-- Create Date:  September 11, 2024
 -- Revision:     1.0
 ----------------------------------------------------------------------------
 
@@ -12,11 +12,65 @@ use ieee.std_logic_1164.all;
 
 entity vending_machine is
 	port (
-		clk		: in	std_ulogic,
-		rst		: in	std_ulogic,
-		nickel	: in	std_ulogic,
-		dime		: in	std_ulogic,
-		dispense	: out	std_ulogic,
+		clk		: in	std_ulogic;
+		rst		: in	std_ulogic;
+		nickel	: in	std_ulogic;
+		dime		: in	std_ulogic;
+		dispense	: out	std_ulogic;
 		amount	: out	natural range 0 to 15
     );
-end entity vending_machine
+end entity vending_machine;
+
+architecture vending_machine_arch of vending_machine is
+
+type State_Type is (Cents0, Cents5, Cents10, Cents15);
+
+Signal	current_state, next_state : State_Type;
+
+begin
+	state_memory : process(clk, rst)
+	begin
+		if (rst = '0') then
+			current_state <= Cents0;
+		elsif (rising_edge(clk)) then
+			current_state <= next_state;
+		end if;
+	end process state_memory;
+	
+	next_state_logic : process(current_state, nickel, dime)
+	begin
+		case (current_state) is
+			when Cents0 => 
+				if (dime = '1') then
+					next_state <= Cents10;
+				elsif (nickel = '1') then
+					next_state <= Cents5;
+				else
+					next_state <= Cents0;
+				end if;
+			when others =>
+				null;
+		end case;
+	end process next_state_logic;
+	
+	output_logic : process(current_state)
+	begin
+		case (current_state) is 
+			when Cents0 =>
+				dispense <= '0';
+				amount <= 0;
+			when Cents5 =>
+				dispense <= '0';
+				amount <= 5;
+			when Cents10 =>
+				dispense <= '0';
+				amount <= 10;
+			when Cents15 =>
+				dispense <= '1';
+				amount <= 15;
+			when others =>
+				null;
+		end case;
+	end process output_logic;
+	
+end architecture vending_machine_arch;
