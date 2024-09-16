36ab193366e61a92d2310bae9c367292b820914c
Grant Kirkland <151691586+SpectrumGrant@users.noreply.github.com>
2024-09-16 11:56:08 -0600

    bug and timing fixes to one pulse code. tested with testbench

--- hdl/async_conditioner/onepulse.vhd
+++ hdl/async_conditioner/onepulse.vhd
@@ -10,14 +10,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity OnePulse is
+entity onepulse is
 	port (clk		: in	std_logic;
 			rst		: in	std_logic;
 			input		: in	std_logic;
 			pulse		: out	std_logic);
 end entity;
 
-architecture OnePulse_arch of OnePulse is 
+architecture onepulse_arch of onepulse is 
 	type State_Type is (standby, output_pulse, lift);
 	signal current_state : State_Type;
 
@@ -29,7 +29,12 @@ begin
 		elsif (rising_edge(clk)) then
 			case (current_state) is
 				when standby => if (input = '1') then current_state <= output_pulse; end if;
-				when output_pulse => current_state <= lift;
+				when output_pulse => 
+					if (input = '0') then
+						current_state <= standby;
+					else
+						current_state <= lift;
+					end if;
 				when lift => if (input = '0') then current_state <= standby; end if;
 			end case;
 		end if;
