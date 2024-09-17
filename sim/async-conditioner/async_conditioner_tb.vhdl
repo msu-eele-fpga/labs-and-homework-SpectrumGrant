b7d14359d46857349887e0bd54f1eb19855435ac
Grant Kirkland <151691586+SpectrumGrant@users.noreply.github.com>
2024-09-16 19:46:24 -0600

    working stimuli generation code

--- sim/async-conditioner/async_conditioner_tb.vhdl
+++ sim/async-conditioner/async_conditioner_tb.vhdl
@@ -8,17 +8,19 @@
 ----------------------------------------------------------------------------
 
 library ieee;
+library osvvm;
 use ieee.std_logic_1164.all;
 use ieee.math_real.all;
 use ieee.numeric_std.all;
 use work.assert_pkg.all;
 use work.print_pkg.all;
 use work.tb_pkg.all;
+use osvvm.randompkg.all;
 
 entity async_conditioner_tb is
 end entity;
 
-architecture async_conditioner_arch of async_conditioner_tb is
+architecture async_conditioner_tb_arch of async_conditioner_tb is
 	signal clk_tb		: std_ulogic := '0';
 	signal rst_tb		: std_ulogic := '0';
 	signal async_tb	: std_ulogic := '0';
@@ -27,7 +29,7 @@ architecture async_conditioner_arch of async_conditioner_tb is
 begin
 	clk_tb <= not clk_tb after CLK_PERIOD / 2;
 	
-	duv : entity work.onepulse
+	duv : entity work.async_conditioner
 		port map (
 			clk	=> clk_tb,
 			rst	=> rst_tb,
@@ -35,10 +37,25 @@ begin
 			sync	=> sync_tb
 		);
 		
+			
 	stimuli_generator : process is
+		variable RV		: RandomPType;
+		variable hold_time : Time;
 	begin
+		RV.InitSeed(RV'instance_name);
+		
 		rst_tb <= '1', '0' after 50 ns;
+		
+		for i in 1 to 40 loop
+			async_tb <= not async_tb;
+			wait for RV.RandTime(10 ms, 400 ms);
+		end loop;	
+		
 		std.env.finish;
 	end process stimuli_generator;
 
+	response_checker : process is
+	
+	end process;
+	
 end architecture;
