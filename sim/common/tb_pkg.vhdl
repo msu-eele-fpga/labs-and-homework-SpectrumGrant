350edcb635af21e8ada51ccec1556d2e0e1fabd2
Grant Kirkland <151691586+SpectrumGrant@users.noreply.github.com>
2024-10-02 12:47:12 -0600

    added alternate clock time constant

--- sim/common/tb_pkg.vhdl
+++ sim/common/tb_pkg.vhdl
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 
 package tb_pkg is 
 	constant CLK_PERIOD : time := 20 ns;
+	constant CLK_PERIOD_SLOW : time := 10 ms;
 	constant COMBINATIONAL_DELAY : time := 1 ns;
 	
 	procedure wait_for_clock_edge(signal clk : in std_ulogic);
