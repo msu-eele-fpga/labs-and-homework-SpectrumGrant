a8b030a0b4dbc089ccf29c65e35f9f6e76f55c7d
Grant Kirkland <151691586+SpectrumGrant@users.noreply.github.com>
2024-10-14 12:28:54 -0600

    bug fixes, added process for boolean conversion. compiles, but untested

--- hdl/led_patterns/led_patterns_avalon.vhdl
+++ hdl/led_patterns/led_patterns_avalon.vhdl
@@ -34,10 +34,52 @@ end entity led_patterns_avalon;
 architecture led_patterns_avalon_arch of led_patterns_avalon is
 
 	signal hps_led_control: std_ulogic_vector(31 downto 0) := "00000000000000000000000000000000";
-	signal base_period: std_ulogic_vector(31 downto 0) := "00000000000000000000000000000000";
+	signal base_period: std_ulogic_vector(31 downto 0) := "00000000000000000000000000010000";
 	signal led_reg : std_ulogic_vector(31 downto 0) := "00000000000000000000000000000000";
 
+	signal bool_conversion : boolean;
+		
+	component led_patterns is
+		generic (
+			system_clock_period : time := 20 ns
+		);
+		port (
+			clk					: in std_ulogic;
+			rst					: in std_ulogic;
+			push_button 		: in std_ulogic;
+			switches				: in std_ulogic_vector(3 downto 0);
+			hps_led_control	: in boolean;
+			base_period 		: in unsigned(7 downto 0);
+			led_reg				: in std_ulogic_vector(7 downto 0);
+			led					: out	std_ulogic_vector(7 downto 0)
+		);
+	end component;
+	
 begin
+	
+	boolean_conversion : process(hps_led_control)
+	begin
+		if hps_led_control(0) then
+			bool_conversion <= true;
+		else
+			bool_conversion <= false;
+		end if;
+	end process;
+	
+	CMP_LED_PATTERNS : led_patterns
+	generic map (
+		system_clock_period => 20 ns
+	)
+	port map (
+		clk => clk,
+		rst => rst,
+		push_button => push_button,
+		switches => switches,
+		hps_led_control => bool_conversion,
+		base_period => unsigned(base_period(7 downto 0)),
+		led_reg => led_reg(7 downto 0),
+		led => led
+	);
 
 	avalon_register_read : process(clk)
 	begin
@@ -51,6 +93,20 @@ begin
 		end if;
 	end process avalon_register_read;
 
+	avalon_register_write : process(clk, rst)
+	begin
+		if (rst = '1') then
+			hps_led_control <= "00000000000000000000000000000000";
+			base_period <= "00000000000000000000000000010000";
+			led_reg <= "00000000000000000000000000000000";
+		elsif (rising_edge(clk) and avs_write = '1') then
+			case avs_address is 
+				when "00" => hps_led_control <= avs_writedata(31 downto 0);
+				when "01" => base_period <= avs_writedata(31 downto 0);
+				when "10" => led_reg <= avs_writedata(31 downto 0);
+				when others => null;
+			end case;
+		end if;
+	end process;
 
-
-end architecture;
+end architecture led_patterns_avalon_arch;
