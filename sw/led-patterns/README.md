22dfd4406aff166a3bd29b45e66d3ae83e758bde
Grant Kirkland <151691586+SpectrumGrant@users.noreply.github.com>
2024-12-10 19:10:31 -0700

    added pattern 4 to led_patterns_miscdev_test

--- sw/led-patterns/README.md
+++ sw/led-patterns/README.md
@@ -1,19 +1,14 @@
-# Usage
+# Utilities
 
-```
-usage: led-patterns [-h] [-v] [-f F | -p P [P ...]]
+## ARM cross-compile environment
 
-Runs through a sequential series of led outputs paired with durations. Accepts both file and command line patern inputs.
+`arm-env.sh` exports some environment variables to make cross-compiling your Linux kernel modules, device trees, etc. easier. It exports `CROSS_COMPILE` and `ARCH`, both of which the Linux kernel build system uses. 
 
-options:
-  -h		    show this help message and exit
-  -v            print additional information with file and pattern commands.
-  -f F          read file and display patterns listed.
-  -p P [P ...]  cycle continuously through pattern duration pairs. Any even number of inputs seperated by spaces.
-  ```
+It also changes the shell prompt to indicate that you are in the arm cross-compiling environment.
 
-# Installation
+> [!IMPORTANT]
+> Any time you need to compile a Linux kernel module or device tree, those environment variables need to be exported! If they aren't, you'll run into issues that might require recompiling the Linux kenrel
 
-The program can be compiled through the following command, and once compiled moved onto the dev board it will be run on.  
-`arm-linux-gnueabihf-gcc -o led-patterns -Wall -static led-patterns.c`  
- This requires the FPGA be programmed with the corresponding LED patterns program, located at `..\..\quartus\LED_Patterns\soc_system.rbf`.
+## Makefile
+
+The Makefile in this folder is used for cross-compiling "normal" C code (i.e., not kenrel modules). It compiles code for x86 and ARM at the same time. This allows you to test your code on your x86 virtual machine, which can be helpful. Testing your code on your virtual machine is only fully possible for code that doesn't access memory-mapped I/O; when using memory-mapped I/O, you'd have to mock or comment-out the memory-mapped I/O operations in order to test your code on an x86 machine.
--- sw/led-patterns/led_patterns_miscdev_test.c
+++ sw/led-patterns/led_patterns_miscdev_test.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 // TODO: update these offsets if your address are different
 #define HPS_LED_CONTROL_OFFSET 0x0
 #define BASE_PERIOD_OFFSET 0x4
 #define LED_REG_OFFSET 0x08
+int pattern_loop();
+void interrupt_handler();
+
+static volatile bool interrupt_triggered = 0;
 
 int main () {
 	FILE *file;
@@ -37,7 +43,7 @@ int main () {
 
 	// Reset file position to 0
 	ret = fseek(file, 0, SEEK_SET);
-	printf("fseek ret = %d\n", ret);
+	printf("fseek ret = %u\n", ret);
 	printf("errno =%s\n", strerror(errno));
 
 
@@ -118,6 +124,66 @@ int main () {
 	ret = fread(&val, 4, 1, file);
 	printf("LED_reg = 0x%x\n", val);
 
+	printf("\n************************************\n");
+	printf(  "**            Looping             **\n");
+	printf(  "************************************\n\n");
+
+	signal(SIGINT, interrupt_handler);
+	pattern_loop(file);
+
 	fclose(file);
 	return 0;
+}
+
+/**
+ * interrupt_handler() - Sets exit flag and returns control to hardware.
+ * 
+ * Triggered on an entry of control-c, setting exit loop boolean for 
+ * pattern, and returning control of the LEDs to the hardware system.
+ */
+void interrupt_handler() {
+	interrupt_triggered = 1;
+}
+
+int pattern_loop(FILE *file) {
+	uint32_t val, prev, hold;
+	size_t ret;	
+	prev = 0x00;
+
+	val = 0x01;
+    ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
+	ret = fwrite(&val, 4, 1, file);
+	fflush(file);
+	printf("HPS_LED_control = 0x%x\n", val);
+
+	val = 0x10;
+    ret = fseek(file, BASE_PERIOD_OFFSET, SEEK_SET);
+	ret = fwrite(&val, 4, 1, file);
+	fflush(file);
+	printf("base period = 0x%x\n", val);
+
+	val = 0x01;
+    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
+	ret = fwrite(&val, 4, 1, file);
+	fflush(file);
+	printf("LED_reg = 0x%x\n", val);
+
+	while (interrupt_triggered == 0) {
+		hold = val;
+		val = val + prev;
+		prev = hold;
+
+		ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
+		ret = fwrite(&val, 4, 1, file);
+		fflush(file);
+		printf("LED_reg = 0x%x\n", val);
+		sleep(1);
+
+	}
+
+	val = 0x00;
+    ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
+	ret = fwrite(&val, 4, 1, file);
+
+	return ret;
 }
